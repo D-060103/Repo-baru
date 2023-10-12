3c1983d9127ccbf5b1a845a07bd61e7d225a721d
David Fischer <davidfischersimanjuntak3@gmail.com>
2023-10-12 16:40:52 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('top')
+@endsection
+
+@section('content')
+
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>{{ \App\Category::count() }}<sup style="font-size: 20px"></sup></h3>
+
+                <p>Category</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-list"></i>
+            </div>
+            <a href="{{ route('categories.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-yellow">
+            <div class="inner">
+                <h3>{{ \App\Product::count() }}</h3>
+                <p>Product</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-cubes"></i>
+            </div>
+            <a href="{{ route('products.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+<!-- Log on to codeastro.com for more projects! -->
+
+@endsection
+
+@section('top')
+@endsection
